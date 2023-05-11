c1cea87636b63b71db8bfef3ae3532dbf0a443f6
mountassirJerrari <mountassir.jerrari@usmba.ac.ma>
2023-05-11 15:24:16 +0100

    //

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Categories from "@/components/Categories";
 import Collections from "@/components/Collections";
 import JoinCard from "@/components/JoinCard";
 import SwiperPanel from "@/components/SwiperPanel";
+import TopRatedRecipes from "@/components/TopRatedRecipes";
 
 
 
@@ -10,12 +11,13 @@ export default function Home() {
     <div>
       <div className="w-full h-screen relative overflow-hidden ">
         <img className="w-full absolute -z-10 " src="/img/swiperBackground.png" alt="" />
-        <SwiperPanel className="mt-32"></SwiperPanel>
+        <SwiperPanel className="mt-32 "></SwiperPanel>
       </div>
       <div className=" h-fit pb-10 rounded-lg border border-gray-300 p-45 my-5 w-4/5 mx-auto flex flex-col  items-center">
        <JoinCard className="mt-5"/>
        <Categories className="mt-5"/>
        <Collections className="mt-5"/>
+       <TopRatedRecipes className="mt-5"/>
       </div>
     </div>
   )
---
+++ components/RecipeCard.tsx
@@ -0,0 +1,35 @@
+import { AiFillHeart } from "react-icons/ai"; 
+import { AiOutlineClockCircle } from "react-icons/ai";
+import clsx from "clsx";
+import { FC, HTMLAttributes } from "react";
+import Rating from "./Rating";
+
+interface RecipeCardProps extends HTMLAttributes<HTMLDivElement> {
+
+    name:string ; 
+    description:string;
+    time:number;
+    rating?:number;
+    imageSrc:string ;
+
+}
+
+const RecipeCard: FC<RecipeCardProps> = ({ className , name , description ,time , imageSrc }) => {
+    return (
+        <div className={clsx("w-60 h-72 overflow-hidden m-4 bg-scondary rounded-xl flex flex-col items-stretch relative", className)}>
+            <AiFillHeart color="FF7E5A" size={20} className=" absolute top-2 right-2 "/>
+
+            <div className="h-2/3 ">
+                <img src={imageSrc} className="object-cover w-full h-full" alt="" />
+            </div>
+            <div className="h-full  px-4 pt-2 pb-5 flex flex-col items-start justify-between ">
+                <div className="font-semibold mb-2 ">{name}</div>
+                <div className="mb-2">{description}</div>
+                <div className="flex justify-between items-center w-full"><Rating color="FF7E5A" size={20} /> <div className="flex justify-center items-center text-sm font-medium"> {time} min <AiOutlineClockCircle className="ml-1" /></div></div>
+
+            </div>
+        </div>
+    );
+}
+
+export default RecipeCard;
